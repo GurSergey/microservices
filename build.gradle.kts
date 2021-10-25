699b6cedb1df0479c3e9ddf1a1781a2109868247
GurSergey <gyrsergey@gmail.com>
2021-10-25 13:19:59 +0400

    added module

---
+++ build.gradle.kts
@@ -0,0 +1,90 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.5.6"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
+}
+
+group = "a"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudGcpVersion"] = "2.0.4"
+extra["springCloudVersion"] = "2020.0.4"
+extra["testcontainersVersion"] = "1.16.0"
+
+dependencies {
+
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-rsocket")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.google.cloud:spring-cloud-gcp-starter-pubsub")
+    implementation("com.google.cloud:spring-cloud-gcp-starter-storage")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.apache.kafka:kafka-streams")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
+    implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
+    implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
+    implementation("org.springframework.cloud:spring-cloud-stream")
+    implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka-streams")
+    implementation("org.springframework.security:spring-security-messaging")
+    implementation("org.springframework.security:spring-security-rsocket")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter") {
+        exclude(group= "commons-logging", module= "commons-logging")
+    }
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner")
+    testImplementation("org.springframework.cloud:spring-cloud-starter-contract-verifier")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:kafka")
+    testImplementation("org.testcontainers:postgresql")
+
+
+
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("com.google.cloud:spring-cloud-gcp-dependencies:${property("springCloudGcpVersion")}")
+        mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ preparation-document-module/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module preparation-document-module {
+    opens net.dol.postcard.order.portal;//class org.springframework.boot.devtools.restart.RestartLauncher cannot access class net.dol.postcard.order.portal.OrderPortalApp
+
+
+
+    //compile dependencies
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+
+
+
+    //spring runtime introspection dependencies
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+
+
+
+    requires jdk.unsupported;//for netty low-level API access
+}
